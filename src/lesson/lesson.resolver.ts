be7b3f5767f85d0439ab2ab82c74a58c98c5c1b6
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-27 16:57:14 +0000

    feat: join of models with graphql

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,13 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
 import { AssignStudentsToLessonInptut } from './assign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { StudentService } from 'src/student/student.service';
 
 @Resolver((of) => LessonType) // defines what this resolver is going to resolve
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
@@ -33,5 +45,26 @@ export class LessonResolver {
   ) {
     const { lessonId, studentIds } = assignStudentToLessonInput;
     return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  /**
+   * we are resolving the field we want to get in the query and also we are getting the data of his parent
+      
+      lesson { 👈 Parent data we are going to get from the decorator
+        name
+        startDate
+        endDate
+        students { 👈 name of the function above the ResolveField decorator
+          firstName
+          lastName
+        }
+      }
+    
+   */
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    console.log(lesson);
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './student.entity';
-import { Repository } from 'typeorm';
+import { MongoRepository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { CreateStudentInput } from './create-student.input';
 
 @Injectable()
 export class StudentService {
   constructor(
-    @InjectRepository(Student) private studentRepository: Repository<Student>,
+    @InjectRepository(Student)
+    private studentRepository: MongoRepository<Student>,
   ) {}
 
   async getStudent(id: string): Promise<Student> {
@@ -17,6 +18,24 @@ export class StudentService {
 
   async getStudents(): Promise<Student[]> {
     return this.studentRepository.find();
+  }
+
+  async getManyStudents(studentIds: string[]): Promise<Student[]> {
+    try {
+      if (typeof studentIds !== 'object' || studentIds.length < 1) {
+        return [];
+      }
+      const students = await this.studentRepository.find({
+        where: { id: { $in: [...studentIds] } },
+      });
+      console.log(students);
+
+      return students;
+    } catch (error) {
+      console.log(error);
+
+      throw new InternalServerErrorException();
+    }
   }
 
   async createStudent(
