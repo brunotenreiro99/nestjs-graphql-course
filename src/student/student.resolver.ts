170381f9dc7a408f3c9b8ce7a74461023b4cac03
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-27 12:55:14 +0000

    feat: added get all students

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { StudentService } from './student.service';
 import { CreateStudentInput } from './create-student.input';
@@ -8,6 +8,11 @@ import { Student } from './student.entity';
 @Resolver((of) => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  async students(): Promise<Student[]> {
+    return this.studentService.getStudents();
+  }
 
   @Mutation((returns) => StudentType)
   async createStudent(
