f8eeb685336136f25ac6a7072738a1d7714757e9
Bruno Tenreiro <b.tenreiro@kigroup.de>
2024-03-27 11:19:31 +0000

    feat: added lesson type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+/*
+    GraphQL can sense the type of the of the variable based on the type provided for the proprety but for the ID its a good practice to specific it as an ID
+*/
+
+@ObjectType('Lesson') // by default the class would be named as LessonType but we can customize it by passing a string
+export class LessonType {
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
